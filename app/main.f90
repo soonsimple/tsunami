dcd3d513ce11791798ff791d861ac3391df3a4fe
soonsimple <sun_chengbo@126.com>
2022-09-06 20:43:49 +0800

    set the initial values of h

--- app/main.f90
+++ app/main.f90
@@ -5,10 +5,12 @@ program tsunami
 
    integer, parameter :: grid_size = 100
    integer, parameter :: num_time_steps = 100
+   integer, parameter :: icenter = 25
 
    real, parameter :: dt = 1. ! time step (s)
    real, parameter :: dx = 1. ! grid spacing (m)
    real, parameter :: c = 1.  ! phase speed (m/s)
+   real, parameter :: decay = 0.02
 
    real, dimension(1:grid_size) :: h, dh
 
@@ -16,6 +18,10 @@ program tsunami
    if (dx <= 0) stop 'grid spacing dx must be > 0.'
    if (c <= 0) stop 'background flow speed c must be > 0.'
    if (grid_size <= 0) stop 'grid size must be >0.'
+
+   do i = 1, grid_size
+      h(i) = exp(-decay*(i - icenter)**2)
+   end do
 
    print *, "hello from project tsunami~"
 
